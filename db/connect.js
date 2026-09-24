ef5524da300e3e80a493416aa549681e6e47fb73
.\.git <.\.gitignore>
2026-09-23 23:58:35 -0300

    connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,38 +1,5 @@
 const dns = require('node:dns');
-const util = require('node:util');
 dns.setServers(['1.1.1.1', '8.8.8.8']);
-
-const dnsPromises = dns.promises;
-const originalLookup = dns.lookup;
-
-async function resolveHostname(hostname) {
-  try {
-    const addresses = await dnsPromises.resolve4(hostname);
-    if (addresses.length > 0) return { address: addresses[0], family: 4 };
-  } catch {
-    // fall through to IPv6
-  }
-  try {
-    const addresses = await dnsPromises.resolve6(hostname);
-    if (addresses.length > 0) return { address: addresses[0], family: 6 };
-  } catch {
-    // fall through to the original OS lookup
-  }
-  return util.promisify(originalLookup)(hostname);
-}
-
-function customLookup(hostname, options, callback) {
-  if (typeof options === 'function') {
-    callback = options;
-    options = {};
-  }
-  resolveHostname(hostname)
-    .then(({ address, family }) => callback(null, address, family))
-    .catch((err) => callback(err));
-}
-customLookup[util.promisify.custom] = resolveHostname;
-
-dns.lookup = customLookup;
 
 const { MongoClient } = require('mongodb');
 
